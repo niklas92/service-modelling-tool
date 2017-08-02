610d8f3268c86966453c05897fe25db34828fe6f
Niklas Scholz <niklas.scholz@tum.de>
2017-08-02 16:48:41 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+
+
+const webpack = require('webpack');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const path = require('path');
+
+module.exports = {
+    entry: {
+        'vendor': ['d3'],
+        'app': path.resolve(__dirname,'src/app.jsx')
+    },
+    output: {
+        path: path.resolve(__dirname,'dist'),
+        filename: 'scripts/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015','react']
+                    }
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [ {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true,
+                        removeComments: false,
+                        collapseWhitespace: false
+                    }
+                }]
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({name: "vendor", minChunks: Infinity,}),
+        new webpack.optimize.UglifyJsPlugin(),
+        new HtmlWebpackPlugin({template: './src/index.html'}),
+        new ExtractTextPlugin("styles/app.css")
+    ]
+
+};
